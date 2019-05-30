ece952ccc614061077cc95b1db9f212d51811c58
Clinton Buehring <cb1416@zeus.cs.txstate.edu>
2019-05-29 20:51:44 -0500

    Updated 052919

--- DDtoDMS.java
+++ DDtoDMS.java
@@ -2,35 +2,68 @@ import java.util.Scanner;
 
 public class DDtoDMS {
 	
+	public static void errorMsg(int a, int b, String c){
+		System.out.println("Please enter a valid " + c);
+		System.out.println("  (Between " + a + " and " + b + ")");
+	}
+
 	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Enter DD for the Lat: ");
-		double lat = sc.nextDouble();
-		System.out.println("Enter DD for the Long: ");
-		double lon = sc.nextDouble();
-		
-		// Convert DD to DMS for Lat
+		double 	lat = 0.0,
+			lon = 0.0;
+		String 	a = "Latitude",
+			b = "Longitude";
+		boolean check = true;
+		boolean negLat = false;
+		boolean negLong = false;
+
+		// Get Decimal Degrees input from user for Latitude.
+		while(check){
+			System.out.println("Enter Decimal Degrees for the Latitude: ");
+			lat = sc.nextDouble();
+			if(lat > 90 || lat < -90){
+				errorMsg(90, -90, a);
+			}else
+				check = false;
+			if(lat < 0 && lat > 90){
+				negLat = true;
+			}
+		}check = true;
+
+		// Get Decimal Degrees input from user for Longitude.
+		while(check){
+			System.out.println("Enter Decimal Degrees for the Longitude: ");
+			lon = sc.nextDouble();
+			if(lon > 180 || lon < -180){
+				errorMsg(180, -180, b);
+			}else
+				check = false;
+			if(lon < 0 && lon > -180){
+				negLong = true;
+			}
+		}check = true;
+
+		// Convert Decimal Degrees to Degrees Minutes Seconds for Latitude.
 		int latD = (int) (lat/1);
 		double rem = lat % 1;
 		int latM = (int) (rem * 60);
 		double rem2 = (rem * 60) % 1;
 		double latS = (rem2 * 60);
+		if(negLat)
+			latD = 0 - latD;
 		
-		// Convert DD to DMS for Long
+		// Convert Decimal Degrees to Degrees Minutes Seconds for Longitude.
 		int longD = (int) (lon/1);
 		double rem3 = lon % 1;
 		int longM = (int) (rem3 * 60);
 		double rem4 = (rem3 * 60) % 1;
 		double longS = (rem4 * 60);
-		
-		System.out.println("Lat Degree: " + latD);
-		System.out.println("Lat Minute: " + latM);
-		System.out.println("Lat Seconds: " + latS);
-		System.out.println("Long Degree: " + longD);
-		System.out.println("Long Minute: " + longM);
-		System.out.println("Long Seconds: " + longS);
-		
+		if(negLong)
+			longD = 0 - longD;
+
+		System.out.println("Latitude: " + latD + "." + latM + "'" + latS + '"');
+		System.out.println("Longitude: " + longD + "." + longM + "'" + longS + '"');
 	}
 }
--- DMStoDD.java
+++ DMStoDD.java
@@ -1,28 +1,106 @@
 import java.util.Scanner;
 
+/*  A simple program to convert a users Degrees Mintues Seconds (DMS) to 
+ *    Decimal Degrees for geographic coordinates.
+*/
+
 public class DMStoDD {
+
+	// Print out error message if user input is out of bounds.
+	public static void errorMsg(int a, int b, String c){
+		System.out.println("\n" + "Please input a correct value for " + c + ".");
+		System.out.println("  (Between " + a + " and " + b + ")");
+	}
 	
+	// Scanner for users input.
 	static Scanner sc = new Scanner(System.in);
 	
-	public static void main(String[] args) {		
-		double latD, latM, latS, longD, longM, longS, lat, lon;
+	public static void main(String[] args) {
+		
+		double 	latD = 0.0, 
+			latM = 0.0, 
+			latS = 0.0, 
+			longD = 0.0, 
+			longM = 0.0, 
+			longS = 0.0, 
+			lat, lon;
+		boolean check = true;		// Boolean for checking users input.
+		boolean negLat = false;		// Boolean to check if Latitude is negative.
+		boolean negLong = false;	// Boolean to check if Longitude is negative.
+		String 	d = "Degrees", 
+			m = "Minutes", 
+			s = "Seconds";
 
-		System.out.print("Please enter the Degree for lat: ");
-		latD = sc.nextDouble();
-		System.out.print("Please enter the Minute for lat: ");
-		latM = sc.nextDouble();
-		System.out.print("Please enter the Second for lat: ");
-		latS = sc.nextDouble();
-		System.out.print("Please enter the Degree for Long: ");
-		longD = sc.nextDouble();
-		System.out.print("Please enter the Minute for Long: ");
-		longM = sc.nextDouble();
-		System.out.print("Please enter the Second for Long: ");
-		longS = sc.nextDouble();
+		System.out.println("********************************************");
+		System.out.println("\n" + "\n" + "Degree Minute Second format: DD.MM'SS" + '"');
+
+		// Obtain users input for Latitude DMS.
+		while(check){
+			System.out.print("Please enter the Degree for Latitude: ");
+			latD = sc.nextDouble();
+			if(latD > 90 || latD < -90){	// Check if within valid range.
+				errorMsg(-90, 90, d);	//  Send error message otherwise.
+			}else
+				check = false;		// Input is valid.
+			if(latD < 0 && latD > -90)	// Check if input is negative.
+				negLat = true;		// Set negative Latitude to true.
+		}check = true;				// Return check to true for next input.
+		while(check){
+			System.out.print("Please enter the Minute for Latitude: ");
+			latM = sc.nextDouble();
+			if(latM > 60 || latM < 0){
+				errorMsg(0, 60, m);
+			}else
+				check = false;
+		}check = true;
+		while(check){
+			System.out.print("Please enter the Second for Latitude: ");
+			latS = sc.nextDouble();
+			if(latS > 60 || latS < 0){
+				errorMsg(0, 60, s);
+			}else
+				check = false;
+		}check = true;
+
+		// Obtain users input for Longitude DMS.
+		while(check){
+			System.out.print("Please enter the Degree for Longitude: ");
+			longD = sc.nextDouble();
+			if(longD > 180 || longD < -180){	// Check if within valid range.
+				errorMsg(-180, 180, d);		//  Send error message otherwise.
+			}else
+				check = false;		// Input is valid
+			if(longD < 0 && longD > -180)	// Check if input is negative
+				negLong = true;		// Set negative Longitude to true.
+		}check = true;				// Return check to true for next input.
+		while(check){
+			System.out.print("Please enter the Minute for Longitude: ");
+			longM = sc.nextDouble();
+			if(longM > 60 || longM < 0){
+				errorMsg(0, 60, m);
+			}else
+				check = false;
+		}check = true;
+		while(check){
+			System.out.print("Please enter the Second for Longitude: ");
+			longS = sc.nextDouble();
+			if(longS > 60 || longS < 0){
+				errorMsg(0, 60, s);
+			}else
+				check = false;
+		}check = true;
 		
-		lat = (latD + (latM/60 + (latS/3600)));
-		lon = (longD + (longM/60 + (longS/3600)));
+		// Take user input and convert to Decimal Degrees
+		if(negLat)
+			lat = (latD - (latM/60 + (latS/3600))); // If negative Lat
+		else
+			lat = (latD + (latM/60 + (latS/3600))); // If positive Lat
+
+		if(negLong)
+			lon = (longD - (longM/60 + (longS/3600)));	// If negative Long.
+		else
+			lon = (longD + (longM/60 + (longS/3600)));	// If positive Long.
 		
-		System.out.println("Lat: " + lat + "  : Long: " + lon);
+		System.out.println("Latitude: " + lat + "  : Longitude: " + lon);
 	}
 }
